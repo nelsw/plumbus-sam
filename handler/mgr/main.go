19d2b5ab0ef1cdd579430f0ffb3e3daa6c0c6e12
Connor Van Elswyk <ConnorVanElswyk@gmail.com>
2021-12-28 00:51:27 -0500

    getting all spends over $100

--- handler/mgr/main.go
+++ handler/mgr/main.go
@@ -1,130 +1,290 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
-	log "github.com/sirupsen/logrus"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
 )
 
 var (
-	db        *dynamodb.Client
-	tableName = "plumbus_fb"
+	mutex      = &sync.Mutex{}
+	digitCheck = regexp.MustCompile(`^[0-9]+$`)
 )
 
-func init() {
-	log.SetOutput(os.Stdout)
-	if cfg, err := config.LoadDefaultConfig(context.TODO()); err != nil {
-		log.WithFields(log.Fields{"err": err}).Error()
-		panic(err)
-	} else {
-		db = dynamodb.NewFromConfig(cfg)
-	}
-}
-
-func handle(request events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
-
-	log.WithFields(log.Fields{"request": request}).Info()
-
-	var body string
-	code := http.StatusBadRequest
-
-	response := events.APIGatewayV2HTTPResponse{
-		Headers:    map[string]string{"Access-Control-Allow-Origin": "*"}, // Required when CORS enabled in API Gateway.
-		StatusCode: code,
-		Body:       body,
-	}
-
-	log.WithFields(log.Fields{"response": response}).Info()
-
-	return response, nil
-}
-
-func put(body string) (string, int) {
-
-	var m map[string]interface{}
-	_ = json.Unmarshal([]byte(body), &m)
-
-	item, _ := attributevalue.MarshalMap(m)
-
-	out, err := db.PutItem(context.TODO(), &dynamodb.PutItemInput{
-		Item:      item,
-		TableName: &tableName,
-	})
-
-	if err != nil {
-		log.WithFields(log.Fields{"err": err}).Error()
-		return "", http.StatusBadRequest
-	}
-
-	_ = attributevalue.UnmarshalMap(out.Attributes, &m)
-
-	bytes, _ := json.Marshal(m)
-
-	return string(bytes), http.StatusOK
-}
-
-func get(ids []string) (string, int) {
-
-	if ids == nil {
-		return "", http.StatusBadRequest
-	}
-
-	if len(ids) == 1 {
-
-		out, err := db.GetItem(context.TODO(), &dynamodb.GetItemInput{
-			Key:       map[string]types.AttributeValue{"id": &types.AttributeValueMemberS{Value: ids[0]}},
-			TableName: &tableName,
-		})
-		if err != nil {
-			log.WithFields(log.Fields{"err": err}).Error()
-			return "", http.StatusBadRequest
-		}
-
-		var m map[string]interface{}
-		_ = attributevalue.UnmarshalMap(out.Item, &m)
-		bytes, _ := json.Marshal(m)
-
-		if len(m) == 0 {
-			return "", http.StatusNotFound
-		}
-
-		return string(bytes), http.StatusOK
-	}
-
-	var keys []map[string]types.AttributeValue
-	for _, id := range ids {
-		keys = append(keys, map[string]types.AttributeValue{"id": &types.AttributeValueMemberS{Value: id}})
-	}
-
-	out, err := db.BatchGetItem(context.TODO(), &dynamodb.BatchGetItemInput{
-		RequestItems: map[string]types.KeysAndAttributes{tableName: {Keys: keys}},
-	})
-	if err != nil {
-		log.WithFields(log.Fields{"err": err}).Error()
-		return "", http.StatusBadRequest
-	}
-
-	var l []map[string]interface{}
-	_ = attributevalue.UnmarshalListOfMaps(out.Responses[tableName], &l)
-	bytes, _ := json.Marshal(l)
-
-	if len(l) == 0 {
-		return "", http.StatusNotFound
-	}
-
-	log.WithFields(log.Fields{"l": l}).Info("l")
-
-	return string(bytes), http.StatusOK
-}
-
 func main() {
-	lambda.Start(handle)
+	getAllAdAccounts()
+}
+
+type AdAccount struct {
+	ID          string  `json:"id"`
+	AccountID   string  `json:"account_id"`
+	Name        string  `json:"name"`
+	Age         float64 `json:"age"`
+	AmountSpent string  `json:"amount_spent"`
+	Status      int     `json:"account_status"`
+}
+
+type Campaign struct {
+	ID        string `json:"id"`
+	AccountID string `json:"account_id"`
+	Name      string `json:"name"`
+	Status    string `json:"status"`
+}
+
+type AdSet struct {
+	CampaignID   string `json:"campaign_id"`
+	CampaignName string `json:"campaign_name"`
+	AccountID    string `json:"account_id"`
+	AccountName  string `json:"account_name"`
+	Spend        string `json:"spend"`
+}
+
+type CampaignGuard struct {
+	FacebookCampaignID string  `json:"facebook_campaign_id"`
+	SovrnCampaignID    string  `json:"sovrn_campaign_id"`
+	Spend              float64 `json:"spend"`
+}
+
+func getAllAdAccounts() {
+
+	base := "https://graph.facebook.com/v12.0/10158615602243295/adaccounts"
+	token := "?access_token=" + os.Getenv("tkn")
+	fields := "&fields=account_id,name,age,amount_spent,account_status"
+	url := base + token + fields
+
+	var err error
+	var data []AdAccount
+
+	if data, err = getAdAccounts(url); err != nil {
+		panic(err)
+	}
+
+	var activeAdAccounts []AdAccount
+	for _, adAccount := range data {
+		if (adAccount.Status == 1 || adAccount.Status == 201) && adAccount.AmountSpent != "0" {
+			activeAdAccounts = append(activeAdAccounts, adAccount)
+		}
+	}
+
+	actives := getActiveCampaigns(activeAdAccounts)
+
+	var f float64
+	var guards []CampaignGuard
+	for accountID, _ := range actives {
+
+		for _, adSet := range getAllAdSetInsights(accountID) {
+
+			if f, err = strconv.ParseFloat(adSet.Spend, 64); err != nil || f < 100 {
+				if err != nil {
+					PrettyPrint(adSet)
+					fmt.Println(err)
+				}
+				continue
+			}
+
+			sovrnCampaignID := adSet.CampaignID
+			if spaced := strings.Split(adSet.CampaignName, " "); len(spaced) > 1 && digitCheck.MatchString(spaced[0]) {
+				sovrnCampaignID = spaced[0]
+			} else if scored := strings.Split(adSet.CampaignName, "_"); len(scored) > 1 && digitCheck.MatchString(scored[0]) {
+				sovrnCampaignID = scored[0]
+			}
+
+			guards = append(guards, CampaignGuard{adSet.CampaignID, sovrnCampaignID, f})
+		}
+	}
+
+	sort.SliceStable(guards, func(i, j int) bool {
+		return guards[i].Spend > guards[j].Spend
+	})
+
+	PrettyPrint(guards)
+	PrettyPrint(len(guards))
+}
+
+func getActiveCampaigns(activeAdAccounts []AdAccount) map[string][]Campaign {
+	var wg sync.WaitGroup
+
+	actives := map[string][]Campaign{}
+	for _, activeAdAccount := range activeAdAccounts {
+		wg.Add(1)
+		go func(activeAdAccount AdAccount) {
+			defer wg.Done()
+			activeCampaigns := getAllCampaignsUnderAccount(activeAdAccount.AccountID)
+			if len(activeCampaigns) > 0 {
+				mutex.Lock()
+				actives[activeAdAccount.AccountID] = activeCampaigns
+				mutex.Unlock()
+			}
+		}(activeAdAccount)
+	}
+
+	wg.Wait()
+
+	return actives
+}
+
+func getAdAccounts(url string) (data []AdAccount, err error) {
+
+	var res *http.Response
+	if res, err = http.Get(url); err != nil {
+		return
+	}
+
+	var body []byte
+	if body, err = ioutil.ReadAll(res.Body); err != nil {
+		return
+	}
+
+	var payload struct {
+		Data []AdAccount `json:"data"`
+		Page struct {
+			Next string `json:"next"`
+		} `json:"paging"`
+	}
+
+	if err = json.Unmarshal(body, &payload); err != nil {
+		return
+	}
+
+	if data = append(payload.Data); payload.Page.Next == "" {
+		return
+	}
+
+	var next []AdAccount
+	if next, err = getAdAccounts(payload.Page.Next); err != nil {
+		return
+	}
+
+	data = append(data, next...)
+	return data, nil
+}
+
+func getAllCampaignsUnderAccount(account string) []Campaign {
+
+	base := "https://graph.facebook.com/v12.0/act_" + account + "/campaigns"
+	token := "?access_token=" + os.Getenv("tkn")
+	fields := "&fields=id,account_id,name,status"
+	url := base + token + fields
+
+	var err error
+	var data []Campaign
+
+	if data, err = getCampaigns(url); err != nil {
+		panic(err)
+	}
+
+	var activeCampaigns []Campaign
+	for _, d := range data {
+		if d.Status == "ACTIVE" {
+			activeCampaigns = append(activeCampaigns, d)
+		}
+	}
+	return activeCampaigns
+}
+
+func getCampaigns(url string) (data []Campaign, err error) {
+
+	var res *http.Response
+	if res, err = http.Get(url); err != nil {
+		return
+	}
+
+	var body []byte
+	if body, err = ioutil.ReadAll(res.Body); err != nil {
+		return
+	}
+
+	var payload struct {
+		Data []Campaign `json:"data"`
+		Page struct {
+			Next string `json:"next"`
+		} `json:"paging"`
+	}
+
+	if err = json.Unmarshal(body, &payload); err != nil {
+		return
+	}
+
+	if data = append(payload.Data); payload.Page.Next == "" {
+		return
+	}
+
+	var next []Campaign
+	if next, err = getCampaigns(payload.Page.Next); err != nil {
+		return
+	}
+
+	data = append(data, next...)
+	return data, nil
+}
+
+func getAllAdSetInsights(adSet string) []AdSet {
+
+	base := "https://graph.facebook.com/v12.0/act_" + adSet + "/insights"
+	token := "?access_token=" + os.Getenv("tkn")
+	fields := "&fields=ad_id,ad_name,adset_id,adset_name,campaign_id,campaign_name,account_id,account_name,spend"
+	date := "&date_preset=today"
+	level := "&level=campaign"
+	url := base + token + fields + date + level
+
+	var err error
+	var data []AdSet
+
+	if data, err = getAdSetInsights(url); err != nil {
+		panic(err)
+	}
+
+	return data
+}
+
+func getAdSetInsights(url string) (data []AdSet, err error) {
+
+	var res *http.Response
+	if res, err = http.Get(url); err != nil {
+		return
+	}
+
+	var body []byte
+	if body, err = ioutil.ReadAll(res.Body); err != nil {
+		return
+	}
+
+	var payload struct {
+		Data []AdSet `json:"data"`
+		Page struct {
+			Next string `json:"next"`
+		} `json:"paging"`
+	}
+
+	if err = json.Unmarshal(body, &payload); err != nil {
+		return
+	}
+
+	if data = append(payload.Data); payload.Page.Next == "" {
+		return
+	}
+
+	var next []AdSet
+	if next, err = getAdSetInsights(payload.Page.Next); err != nil {
+		return
+	}
+
+	data = append(data, next...)
+	return data, nil
+}
+
+func Pretty(v interface{}) string {
+	b, _ := json.MarshalIndent(v, "", "    ")
+	return string(b)
+}
+
+func PrettyPrint(v interface{}) {
+	fmt.Println(Pretty(v))
 }
