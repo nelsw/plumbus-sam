613a2d82f477776f9275070b40f80ddfc0565fc9
Connor Van Elswyk <ConnorVanElswyk@gmail.com>
2022-01-20 04:25:47 -0500

    small refactor, all tests passing

--- handler/agg/main_test.go
+++ handler/agg/main_test.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"context"
 	"net/http"
 	"plumbus/pkg/sam"
+	"plumbus/test"
 	"testing"
 )
 
 var (
-	ctx             = context.TODO()
 	rootParam       = map[string]string{"node": "root"}
 	accountParam    = map[string]string{"node": "account"}
 	campaignParam   = map[string]string{"node": "campaign"}
@@ -16,49 +15,49 @@ var (
 )
 
 func TestPutAccounts(t *testing.T) {
-	if res, _ := handle(ctx, sam.NewRequest(http.MethodPut, accountParam)); res.StatusCode != http.StatusOK {
+	if res, _ := handle(test.CTX, sam.NewRequest(http.MethodPut, accountParam)); res.StatusCode != http.StatusOK {
 		t.Error(res.StatusCode, res.Body)
 	}
 }
 
 func TestPutCampaigns(t *testing.T) {
-	if res, _ := handle(ctx, sam.NewRequest(http.MethodPut, campaignParam)); res.StatusCode != http.StatusOK {
+	if res, _ := handle(test.CTX, sam.NewRequest(http.MethodPut, campaignParam)); res.StatusCode != http.StatusOK {
 		t.Error(res.StatusCode, res.Body)
 	}
 }
 
 func TestPostRoot(t *testing.T) {
-	if res, _ := handle(ctx, sam.NewRequest(http.MethodPost, rootParam)); res.StatusCode != http.StatusOK {
+	if res, _ := handle(test.CTX, sam.NewRequest(http.MethodPost, rootParam)); res.StatusCode != http.StatusOK {
 		t.Error(res.StatusCode, res.Body)
 	}
 }
 
 func TestPostAccounts(t *testing.T) {
-	if res, _ := handle(ctx, sam.NewRequest(http.MethodPost, accountParam)); res.StatusCode != http.StatusOK {
+	if res, _ := handle(test.CTX, sam.NewRequest(http.MethodPost, accountParam)); res.StatusCode != http.StatusOK {
 		t.Error(res.StatusCode, res.Body)
 	}
 }
 
 func TestPostCampaigns(t *testing.T) {
-	if res, _ := handle(ctx, sam.NewRequest(http.MethodPost, campaignParam)); res.StatusCode != http.StatusOK {
+	if res, _ := handle(test.CTX, sam.NewRequest(http.MethodPost, campaignParam)); res.StatusCode != http.StatusOK {
 		t.Error(res.StatusCode, res.Body)
 	}
 }
 
 func TestGetRoot(t *testing.T) {
-	if res, _ := handle(ctx, sam.NewRequest(http.MethodGet, rootParam)); res.StatusCode != http.StatusOK {
+	if res, _ := handle(test.CTX, sam.NewRequest(http.MethodGet, rootParam)); res.StatusCode != http.StatusOK {
 		t.Error(res.StatusCode, res.Body)
 	}
 }
 
 func TestGetAccounts(t *testing.T) {
-	if res, _ := handle(ctx, sam.NewRequest(http.MethodGet, accountParam)); res.StatusCode != http.StatusOK {
+	if res, _ := handle(test.CTX, sam.NewRequest(http.MethodGet, accountParam)); res.StatusCode != http.StatusOK {
 		t.Error(res.StatusCode, res.Body)
 	}
 }
 
 func TestGetCampaigns(t *testing.T) {
-	if res, _ := handle(ctx, sam.NewRequest(http.MethodGet, campaignIDParam)); res.StatusCode != http.StatusOK {
+	if res, _ := handle(test.CTX, sam.NewRequest(http.MethodGet, campaignIDParam)); res.StatusCode != http.StatusOK {
 		t.Error(res.StatusCode, res.Body)
 	}
 }
