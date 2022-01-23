fda280b0fc32fecfd567469ceca4c37043c3f595
Connor Van Elswyk <ConnorVanElswyk@gmail.com>
2022-01-23 12:01:32 -0500

    new util method

--- pkg/util/nums/nums.go
+++ pkg/util/nums/nums.go
@@ -1,6 +1,7 @@
 package nums
 
 import (
+	"reflect"
 	"strconv"
 )
 
@@ -15,4 +16,22 @@ func IsNotNumber(s string) bool {
 func isNumber(s string) bool {
 	_, err := strconv.ParseFloat(s, 64)
 	return err == nil
+}
+
+func Float64(v interface{}) float64 {
+	if v == nil {
+		return 0
+	} else if k := reflect.ValueOf(v).Kind(); k == reflect.String {
+		if s := v.(string); s == "" {
+			return 0
+		} else if f, err := strconv.ParseFloat(s, 64); err != nil {
+			return 0
+		} else {
+			return f
+		}
+	} else if k == reflect.Float64 {
+		return v.(float64)
+	} else {
+		return 0
+	}
 }
