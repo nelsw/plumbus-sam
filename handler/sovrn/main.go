d2092f4f6bee7abf01d99979e866bf7ccd329984
Connor Van Elswyk <ConnorVanElswyk@gmail.com>
2022-01-23 12:04:36 -0500

    sovrn inclusion of arbo and small refactors

--- handler/sovrn/main.go
+++ handler/sovrn/main.go
@@ -5,41 +5,18 @@ import (
 	"encoding/json"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	log "github.com/sirupsen/logrus"
 	"net/http"
 	"plumbus/pkg/api"
+	"plumbus/pkg/model/account"
+	"plumbus/pkg/model/arbo"
+	"plumbus/pkg/model/sovrn"
 	"plumbus/pkg/repo"
 	"plumbus/pkg/sam"
 	"plumbus/pkg/util/logs"
 	"strings"
 )
-
-const table = "plumbus_fb_sovrn"
-
-type payload struct {
-	Attachment struct {
-		Data string `json:"data"`
-	} `json:"attachment"`
-}
-
-type value struct {
-	UTM         string  `json:"impressions.utm_campaign"`
-	Revenue     float64 `json:"impressions.estimated_revenue"`
-	Impressions int     `json:"impressions.total_ad_impressions"`
-	Sessions    int     `json:"impressions.total_sessions"`
-	CTR         float64 `json:"impressions.click_through_rate"`
-	PageViews   int     `json:"impressions.total_page_views"`
-}
-
-func (v value) writeRequest() (out types.WriteRequest, err error) {
-	var item map[string]types.AttributeValue
-	if item, err = attributevalue.MarshalMap(&v); err == nil {
-		out.PutRequest = &types.PutRequest{Item: item}
-	}
-	return
-}
 
 func init() {
 	logs.Init()
@@ -49,8 +26,24 @@ func handle(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.API
 
 	log.WithFields(log.Fields{"ctx": ctx, "req": req}).Info()
 
+	data := sam.NewRequestBytes(http.MethodPut, nil)
+	arboSuccess := true
+	if _, err := sam.NewReqRes(ctx, arbo.Handler, data); err != nil {
+		log.WithError(err).Error("while invoking request response from arbo handler")
+		arboSuccess = false
+	}
+
+	sovrnSuccess := true
 	if err := process(ctx, req); err != nil {
 		log.WithError(err).Error("while processing sovrn request")
+		sovrnSuccess = false
+	}
+
+	if arboSuccess || sovrnSuccess {
+		data = sam.NewRequestBytes(http.MethodPost, nil)
+		if _, err := sam.NewEvent(ctx, account.Handler, data); err != nil {
+			log.WithError(err).Error("while invoking account post event")
+		}
 	}
 
 	// as sovrn is actively hitting this webhook,
@@ -61,7 +54,7 @@ func handle(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.API
 
 func process(ctx context.Context, request events.APIGatewayV2HTTPRequest) (err error) {
 
-	var pay payload
+	var pay sovrn.Payload
 	if err = json.Unmarshal([]byte(strings.TrimLeft(request.Body, "attachment")), &pay); err != nil {
 		log.WithError(err).Error("unable to interpret request.Body attachment payload")
 		return
@@ -69,7 +62,7 @@ func process(ctx context.Context, request events.APIGatewayV2HTTPRequest) (err e
 
 	log.Trace("unmarshalled sovrn payload from request.Body attachment")
 
-	var vv []value
+	var vv []sovrn.Value
 	if err = json.Unmarshal([]byte(pay.Attachment.Data), &vv); err != nil {
 		log.WithError(err).Error("unable to unmarshal payload attachment data into sovrn value slice")
 		return err
@@ -80,14 +73,14 @@ func process(ctx context.Context, request events.APIGatewayV2HTTPRequest) (err e
 	// as each sovrn value represents a campaign,
 	// we group sovrn values by (campaign) UTM
 	// to sum and average value data points.
-	groups := map[string][]value{}
+	groups := map[string][]sovrn.Value{}
 	for _, v := range vv {
 		if v.UTM == "" {
 			log.Trace("value missing campaign id", v)
 		} else if _, ok := groups[v.UTM]; ok {
 			groups[v.UTM] = append(groups[v.UTM], v)
 		} else {
-			groups[v.UTM] = []value{v}
+			groups[v.UTM] = []sovrn.Value{v}
 		}
 	}
 
@@ -100,7 +93,7 @@ func process(ctx context.Context, request events.APIGatewayV2HTTPRequest) (err e
 	var rr []types.WriteRequest
 	for utm, group := range groups {
 
-		v := value{UTM: utm}
+		v := sovrn.Value{UTM: utm}
 
 		for _, g := range group {
 			v.Revenue += g.Revenue
@@ -112,24 +105,19 @@ func process(ctx context.Context, request events.APIGatewayV2HTTPRequest) (err e
 
 		v.CTR /= float64(len(group))
 
-		if r, err = v.writeRequest(); err != nil {
+		if r, err = v.WriteRequest(); err != nil {
 			log.WithError(err).Error("sovrn value to write request")
 		} else {
 			rr = append(rr, r)
 		}
 	}
 
-	if err = repo.BatchWrite(ctx, table, rr); err != nil {
+	if err = repo.BatchWrite(ctx, sovrn.Table, rr); err != nil {
 		log.WithError(err).Error("sovrn value batch write items")
 		return
 	}
 
 	log.WithFields(log.Fields{"size": len(rr)}).Trace("sovrn value batch write items")
-
-	data := sam.NewRequestBytes(http.MethodPost, map[string]string{"node": "root"})
-	if _, err = sam.NewEvent(ctx, "plumbus_aggHandler", data); err != nil {
-		log.WithError(err).Error("while invoking agg event")
-	}
 
 	return
 }
--- pkg/model/sovrn/sovrn.go
+++ pkg/model/sovrn/sovrn.go
@@ -1,10 +1,11 @@
 package sovrn
 
-var table = "plumbus_fb_sovrn"
+import (
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+)
 
-func Table() string {
-	return table
-}
+var Table = "plumbus_fb_sovrn"
 
 type Entity struct {
 	UTM         string  `json:"UTM"`
@@ -13,4 +14,27 @@ type Entity struct {
 	Sessions    int     `json:"Sessions"`
 	CTR         float64 `json:"CTR"`
 	PageViews   int     `json:"PageViews"`
+}
+
+type Payload struct {
+	Attachment struct {
+		Data string `json:"data"`
+	} `json:"attachment"`
+}
+
+type Value struct {
+	UTM         string  `json:"impressions.utm_campaign"`
+	Revenue     float64 `json:"impressions.estimated_revenue"`
+	Impressions int     `json:"impressions.total_ad_impressions"`
+	Sessions    int     `json:"impressions.total_sessions"`
+	CTR         float64 `json:"impressions.click_through_rate"`
+	PageViews   int     `json:"impressions.total_page_views"`
+}
+
+func (v *Value) WriteRequest() (out types.WriteRequest, err error) {
+	var item map[string]types.AttributeValue
+	if item, err = attributevalue.MarshalMap(&v); err == nil {
+		out.PutRequest = &types.PutRequest{Item: item}
+	}
+	return
 }
