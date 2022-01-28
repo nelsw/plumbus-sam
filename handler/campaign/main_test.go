0727621e18ae8a9e7462f92bb1ffd623cdad1cf4
Connor Van Elswyk <ConnorVanElswyk@gmail.com>
2022-01-28 03:09:58 -0500

    uncommented test methods

--- handler/campaign/main_test.go
+++ handler/campaign/main_test.go
@@ -1,45 +1,34 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
-	"plumbus/pkg/model/campaign"
 	"plumbus/pkg/sam"
-	"plumbus/pkg/util/pretty"
 	"plumbus/test"
 	"testing"
 )
 
 func TestHandlePut(t *testing.T) {
-	req := sam.NewRequest(http.MethodPut, map[string]string{"accountID": "264100649065412"})
+	req := sam.NewRequest(http.MethodPut, map[string]string{"accountID": "2069406016568961"})
 	if res, _ := handle(test.CTX, req); res.StatusCode != http.StatusOK {
 		t.Error(res.StatusCode, res.Body)
-	} else {
-		var cc []campaign.Entity
-		json.Unmarshal([]byte(res.Body), &cc)
-		pretty.Print(cc)
 	}
 }
 
-//func TestHandleGetByAccountID(t *testing.T) {
-//	par := map[string]string{"accountID": "1450566098533975"}
-//	req := sam.NewRequest(http.MethodGet, par)
-//	if res, _ := handle(test.CTX, req); res.StatusCode != http.StatusOK {
-//		t.Error(res.StatusCode, res.Body)
-//	} else {
-//		var cc []campaign.Entity
-//		json.Unmarshal([]byte(res.Body), &cc)
-//		pretty.Print(cc)
-//	}
-//}
-//
-//func TestHandleGetByAccountIDAndCampaignIDS(t *testing.T) {
-//	par := map[string]string{
-//		"accountID":   "544877570187911",
-//		"campaignIDS": "23849761526340551,23849761526450551",
-//	}
-//	req := sam.NewRequest(http.MethodGet, par)
-//	if res, _ := handle(test.CTX, req); res.StatusCode != http.StatusOK {
-//		t.Error(res.StatusCode, res.Body)
-//	}
-//}
+func TestHandleGetByAccountID(t *testing.T) {
+	par := map[string]string{"accountID": "1450566098533975"}
+	req := sam.NewRequest(http.MethodGet, par)
+	if res, _ := handle(test.CTX, req); res.StatusCode != http.StatusOK {
+		t.Error(res.StatusCode, res.Body)
+	}
+}
+
+func TestHandleGetByAccountIDAndCampaignIDS(t *testing.T) {
+	par := map[string]string{
+		"accountID":   "544877570187911",
+		"campaignIDS": "23849761526340551,23849761526450551",
+	}
+	req := sam.NewRequest(http.MethodGet, par)
+	if res, _ := handle(test.CTX, req); res.StatusCode != http.StatusOK {
+		t.Error(res.StatusCode, res.Body)
+	}
+}
